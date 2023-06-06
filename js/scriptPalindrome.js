78ee624453868555cc4298baa1ab5c1cda2429d9
marcocareddu <saldumaccu@tuta.io>
2023-06-06 15:32:58 +0200

    JS Palindrome - Get user input on click

--- js/scriptPalindrome.js
+++ js/scriptPalindrome.js
@@ -6,3 +6,9 @@ console.log('JS PALINDROME OK');
 // Link to DOM Elements
 const userInput = document.getElementById('user-input');
 const button = document.querySelector('button');
+
+// Get user input on click
+button.addEventListener('click', function () {
+    const word = userInput.value;
+    console.log(word);
+})
