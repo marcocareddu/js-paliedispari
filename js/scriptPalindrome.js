f1725a370c9290d35f2035772b258ff3536fb0e3
marcocareddu <saldumaccu@tuta.io>
2023-06-06 15:28:12 +0200

    JS Palindrome -Linked DOM elements

--- js/scriptPalindrome.js
+++ js/scriptPalindrome.js
@@ -1,1 +1,8 @@
 console.log('JS PALINDROME OK');
+
+// ? ****************************** PALINDROME ******************************
+
+// * PHASE 1
+// Link to DOM Elements
+const userInput = document.getElementById('user-input');
+const button = document.querySelector('button');
