9185b1be60f20650aafb5fe29f7fd5f76da32311
marcocareddu <saldumaccu@tuta.io>
2023-06-06 17:51:21 +0200

    JS Even Odd - Created random function

--- js/scriptDice.js
+++ js/scriptDice.js
@@ -3,13 +3,17 @@ console.log('JS DICE OK');
 // ? ******************************* EVEN ODD *******************************
 // * Phase 1
 // Link to DOM element
-const button = document.getElementById('even-odd-btn');
+const evenOddButton = document.getElementById('even-odd-btn');
 const selectInput = document.querySelector('select');
 const numberInput = document.getElementById('user-number');
 
 // Create var
 let cpuNumber;
 let sumNumber;
-let result;
+let evenOddResult;
 
-
+// Create random function
+function random() {
+    const random = Math.floor(Math.random() * 5) + 1;
+    return random;
+}
