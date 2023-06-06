a668abc3d2f579e9916db75ec4b97cc40428d197
marcocareddu <saldumaccu@tuta.io>
2023-06-06 17:42:05 +0200

    JS EvenOdd - Link to DOM element & created var

--- js/scriptDice.js
+++ js/scriptDice.js
@@ -1,1 +1,15 @@
 console.log('JS DICE OK');
+
+// ? ******************************* EVEN ODD *******************************
+// * Phase 1
+// Link to DOM element
+const button = document.getElementById('even-odd-btn');
+const selectInput = document.querySelector('select');
+const numberInput = document.getElementById('user-number');
+
+// Create var
+let cpuNumber;
+let sumNumber;
+let result;
+
+
